bee7bff3756b8a2519c86538d63727f2c96a0717
andrey.krotkikh <andrey.krotkikh@aliexpress.ru>
2022-11-05 04:30:56 +0300

    desc

--- utils/get_data.py
+++ utils/get_data.py
@@ -10,28 +10,16 @@ all_projects_dict = request_all_projects.json()
 all_projects = {project['_id']: project for project in all_projects_dict}
 
 def get_user(id: str) -> Dict[str, Any]:
-    # request = requests.get(f"https://space-bear-api.herokuapp.com/api/users/ml-get-by-id/{id}")
-    # json_dict = request.json()
-    # return json_dict
     return all_users[id]
 
 
 def get_all_users() -> List[Dict[str, Any]]:
-    # request = requests.get("https://space-bear-api.herokuapp.com/api/users/ml-get-all")
-    # json_dict = request.json()
-    # return json_dict
     return list(all_users.values())
 
 
 def get_project(id: str) -> Dict[str, Any]:
-    # request = requests.get(f"https://space-bear-api.herokuapp.com/api/projects/ml-get-by-id/{id}")
-    # json_dict = request.json()
-    # return json_dict
     return all_projects[id]
 
 
 def get_all_projects() -> List[Dict[str, Any]]:
-    # request = requests.get("https://space-bear-api.herokuapp.com/api/projects/ml-get-all")
-    # json_dict = request.json()
-    # return json_dict
     return list(all_projects.values())
